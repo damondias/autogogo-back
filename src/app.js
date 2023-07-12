5a4e5d23c2988fcb75d48da430e9c52cb09b3616
Damon <damondias@gmail.com>
2023-07-12 08:37:45 -0400

    Installing packages and basic architecture of the project

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express, { json } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import router from "./routes/index.routes";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+app.use(router);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`
+ Running API AutoGoGo on port ${PORT},
+ Url: http://localhost:${PORT} 
+`));
