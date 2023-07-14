cb3b9d87797ae8d7dbe6b30457f739e921c57973
Levy <levymcruz@gmail.com>
2023-07-14 15:54:49 -0300

    feat: creating ORM to db basic operations

--- testPost.js
+++ testPost.js
@@ -1,18 +1,29 @@
 import axios from 'axios'
 import dayjs from 'dayjs'
+import ORM from './src/middlewares/orm.js'
+import { db } from './src/database/database.connection.js';
 
-axios.post('http://localhost:5000/home', {
-    nome: "Gol",
-    marca: "Volkswagen",
-    modelo: "Gol",
-    ano: 2010,
-    km: 100000,
-    qntDonos: 2,
-    infoExtra: "Sofreu apenas um acidente, mas o carro concertou e funciona perfeitamente",
-    valor: 35000,
-    insertedDate: dayjs().format('DD/MM/YYYY HH:mm:s'),
-    createdBy: "id do usuário que tava logado e que inseriu"}).then(res =>{
-    console.log(res.data)
-}).catch(err => {
-    console.log(err.response.data)
-})
+// axios.post('http://localhost:5000/home', {
+//     nome: "Gol",
+//     marca: "Volkswagen",
+//     modelo: "Gol",
+//     ano: 2010,
+//     km: 100000,
+//     qntDonos: 2,
+//     infoExtra: "Sofreu apenas um acidente, mas o carro concertou e funciona perfeitamente",
+//     valor: 35000,
+//     insertedDate: dayjs().format('DD/MM/YYYY HH:mm:s'),
+//     createdBy: "id do usuário que tava logado e que inseriu"}).then(res =>{
+//     console.log(res.data)
+// }).catch(err => {
+//     console.log(err.response.data)
+// })
+
+
+const orm = new ORM(db);
+
+
+const insert = () => {
+    const id = orm.create('teste', { nome: 'Levy' });
+}
+insert()
